2433b4d9f66fa6cd0ba3c530e185301310573f4b
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-09-26 11:00:33 +0900

    Initial Commit

---
+++ google_api/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  google_api
+//
+//  Created by 中村祐太 on 2016/09/26.
+//  Copyright © 2016年 中村祐太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
